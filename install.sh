60c2e59b048735e876420663434991e7534e8715
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2025-01-13 22:54:46 -0300

    refactor: improvement in installation script logs.

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ BIN_PATH="$INSTALL_DIR/$PROGRAM_NAME"
 DB_DIR="/etc/$PROGRAM_NAME"
 DB_FILE="$DB_DIR/inventory.json"
 SRC_DIR="./"
-BIN_PATH="$INSTALL_DIR/$PROGRAM_NAME"
 
 echo "Starting the installation of $PROGRAM_NAME..."
 
@@ -16,24 +15,27 @@ if ! command -v go &> /dev/null; then
 fi
 
 echo "Compiling..."
-go build -o "$PROGRAM_NAME" "$SRC_DIR/main.go"
-
-if [ ! -f "$PROGRAM_NAME" ]; then
+if ! go build -o "$PROGRAM_NAME" "$SRC_DIR/main.go"; then
   echo "Go code compilation failed. Check for errors."
   exit 1
 fi
 
 echo "Copying files..."
-
 sudo cp "$PROGRAM_NAME" "$BIN_PATH"
 sudo chmod +x "$BIN_PATH"
 
 if [ ! -f "$DB_FILE" ]; then
   echo "Creating cache file..."
-
-  sudo mkdir -p "$DB_DIR"
-    
-  echo '' | sudo tee "$DB_FILE" > /dev/null
+  
+  if ! sudo mkdir -p "$DB_DIR"; then
+    echo "Failed to create directory $DB_DIR. Check permissions."
+    exit 1
+  fi
+  
+  if ! echo '{}' | sudo tee "$DB_FILE" > /dev/null; then
+    echo "Failed to create JSON file $DB_FILE. Check permissions."
+    exit 1
+  fi
 
   sudo chmod 644 "$DB_FILE"
   sudo chown $USER:$USER "$DB_FILE"
@@ -43,11 +45,9 @@ else
   echo "The cache file already exists. No action was taken."
 fi
 
-sudo chmod 644 "$DB_FILE"
-sudo chown $USER:$USER "$DB_FILE"
-
 if [ -f "$BIN_PATH" ] && [ -f "$DB_FILE" ]; then
   echo "$PROGRAM_NAME successfully installed!"
 else
   echo "Installation error!"
+  exit 1
 fi
